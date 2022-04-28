62e0d128a74c7ce90e863b50fd831f3fb030348e
Rockwell Schrock <rockwell@schrock.me>
2022-04-27 22:14:39 -0400

    Working on alerts

--- lib/hamsat_web/live/alerts/index_live.ex
+++ lib/hamsat_web/live/alerts/index_live.ex
@@ -1,6 +1,8 @@
 defmodule HamsatWeb.Alerts.IndexLive do
   use HamsatWeb, :live_view
 
+  import HamsatWeb.LayoutComponents
+
   alias Hamsat.Alerts
 
   def mount(_params, _session, socket) do
@@ -12,15 +14,19 @@ defmodule HamsatWeb.Alerts.IndexLive do
     filter = [date: parse_date_filter(params)]
     alerts = Alerts.list_alerts(socket.assigns.context, filter)
 
-    browse_link_to =
-      if filter[:date] == :upcoming,
-        do: Routes.alerts_path(socket, :index, date: Date.utc_today() |> Date.to_iso8601())
+    duration = if filter[:date] == :upcoming, do: :upcoming, else: :browse
+
+    # browse_link_to =
+    #   if filter[:date] == :upcoming,
+    #     do: Routes.alerts_path(socket, :index, date: Date.utc_today() |> Date.to_iso8601())
 
     socket =
       socket
       |> assign(:filter, filter)
       |> assign(:alerts, alerts)
-      |> assign(:browse_link_to, browse_link_to)
+      |> assign(:duration, duration)
+
+    # |> assign(:browse_link_to, browse_link_to)
 
     {:noreply, socket}
   end
@@ -34,7 +40,15 @@ defmodule HamsatWeb.Alerts.IndexLive do
     end
   end
 
-  def handle_event("filter-form-changed", params, socket) do
+  def handle_event("select", %{"id" => "interval", "selected" => "upcoming"}, socket) do
+    {:noreply, push_patch(socket, to: Routes.alerts_path(socket, :index))}
+  end
+
+  def handle_event("select", %{"id" => "interval", "selected" => "browse"}, socket) do
+    {:noreply, push_patch(socket, to: browse_url(socket))}
+  end
+
+  def handle_event("date-changed", params, socket) do
     params = Map.take(params, ["date"])
     socket = push_patch(socket, to: Routes.alerts_path(socket, :index, params))
 
@@ -44,5 +58,14 @@ defmodule HamsatWeb.Alerts.IndexLive do
   def handle_info(:set_now, socket) do
     Process.send_after(self(), :set_now, 1_000)
     {:noreply, assign(socket, :now, DateTime.utc_now())}
+  end
+
+  defp duration_options do
+    [upcoming: "Upcoming", browse: "Browse"]
+  end
+
+  defp browse_url(socket) do
+    default_date = Date.utc_today() |> Timex.shift(days: 1) |> Date.to_iso8601()
+    Routes.alerts_path(socket, :index, date: default_date)
   end
 end
