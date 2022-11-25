b3add64171434060b4edebd2f86868a7343c8880
Rockwell Schrock <rockwell@schrock.me>
2022-11-24 23:12:01 -0500

    Add sat uplink data

--- lib/hamsat/satellites/satellites.ex
+++ lib/hamsat/satellites/satellites.ex
@@ -38,7 +38,11 @@ defmodule Hamsat.Satellites do
     Repo.get_by!(Sat, number: number)
   end
 
-  # SOURCE: https://www.amsat.org/two-way-satellites/
+  #
+  # SOURCES:
+  # https://www.amsat.org/two-way-satellites/
+  # https://www.amsat.org/linear-satellite-frequency-summary/
+  #
   def known do
     [
       %{
@@ -49,117 +53,139 @@ defmodule Hamsat.Satellites do
         downlinks: [
           %{lower_mhz: 29.4000, upper_mhz: 29.5000},
           %{lower_mhz: 145.925, upper_mhz: 145.975}
+        ],
+        uplinks: [
+          %{lower_mhz: 145.850, upper_mhz: 145.950},
+          %{lower_mhz: 432.125, upper_mhz: 432.175}
         ]
       },
       %{
         name: "AO-27",
         number: 22825,
         modulation: :fm,
-        downlinks: [%{lower_mhz: 436.795, upper_mhz: 436.795}]
+        downlinks: [%{lower_mhz: 436.795, upper_mhz: 436.795}],
+        uplinks: [%{lower_mhz: 145.850, upper_mhz: 145.850}]
       },
       %{
         name: "AO-73",
         number: 39444,
         modulation: :linear,
-        downlinks: [%{lower_mhz: 145.950, upper_mhz: 145.970}]
+        downlinks: [%{lower_mhz: 145.950, upper_mhz: 145.970}],
+        uplinks: [%{lower_mhz: 435.130, upper_mhz: 435.150}]
       },
       %{
         name: "AO-91",
         number: 43017,
         modulation: :fm,
-        downlinks: [%{lower_mhz: 145.96, upper_mhz: 145.96}]
+        downlinks: [%{lower_mhz: 145.96, upper_mhz: 145.96}],
+        uplinks: [%{lower_mhz: 435.250, upper_mhz: 435.250}]
       },
       %{
         name: "CAS-4A",
         number: 42761,
         modulation: :linear,
-        downlinks: [%{lower_mhz: 145.860, upper_mhz: 145.880}]
+        downlinks: [%{lower_mhz: 145.860, upper_mhz: 145.880}],
+        uplinks: [%{lower_mhz: 435.210, upper_mhz: 435.230}]
       },
       %{
         name: "CAS-4B",
         number: 42759,
         modulation: :linear,
-        downlinks: [%{lower_mhz: 145.915, upper_mhz: 145.935}]
+        downlinks: [%{lower_mhz: 145.915, upper_mhz: 145.935}],
+        uplinks: [%{lower_mhz: 435.270, upper_mhz: 435.290}]
       },
       %{
         name: "FO-29",
         number: 24278,
         modulation: :linear,
-        downlinks: [%{lower_mhz: 435.800, upper_mhz: 435.900}]
+        downlinks: [%{lower_mhz: 435.800, upper_mhz: 435.900}],
+        uplinks: [%{lower_mhz: 145.900, upper_mhz: 146.000}]
       },
       %{
         name: "FO-99",
         number: 43937,
         modulation: :linear,
-        downlinks: [%{lower_mhz: 145.900, upper_mhz: 145.930}]
+        downlinks: [%{lower_mhz: 435.880, upper_mhz: 435.910}],
+        uplinks: [%{lower_mhz: 145.900, upper_mhz: 145.930}]
       },
       %{
         name: "GREENCUBE",
         number: 53106,
         modulation: :digital,
-        downlinks: [%{lower_mhz: 435.310, upper_mhz: 435.310}]
+        downlinks: [%{lower_mhz: 435.310, upper_mhz: 435.310}],
+        uplinks: [%{lower_mhz: 435.310, upper_mhz: 435.310}]
       },
       %{
         name: "ISS",
         number: 25544,
         modulation: :fm,
-        downlinks: [%{lower_mhz: 437.8, upper_mhz: 437.8}]
+        downlinks: [%{lower_mhz: 437.8, upper_mhz: 437.8}],
+        uplinks: [%{lower_mhz: 145.990, upper_mhz: 145.990}]
       },
       %{
         name: "JO-97",
         number: 43803,
         modulation: :linear,
-        downlinks: [%{lower_mhz: 145.855, upper_mhz: 145.875}]
+        downlinks: [%{lower_mhz: 145.855, upper_mhz: 145.875}],
+        uplinks: [%{lower_mhz: 435.100, upper_mhz: 435.120}]
       },
       %{
         name: "LilacSat-2",
         nasa_name: "LILACSAT-2",
         number: 40908,
         modulation: :fm,
-        downlinks: [%{lower_mhz: 437.2, upper_mhz: 437.2}]
+        downlinks: [%{lower_mhz: 437.2, upper_mhz: 437.2}],
+        uplinks: [%{lower_mhz: 144.350, upper_mhz: 144.350}]
       },
       # %{name: "MO-112", number: 48868, modulation: :fm},
       %{
         name: "PO-101",
         number: 43678,
         modulation: :fm,
-        downlinks: [%{lower_mhz: 145.9, upper_mhz: 145.9}]
+        downlinks: [%{lower_mhz: 145.9, upper_mhz: 145.9}],
+        uplinks: [%{lower_mhz: 437.500, upper_mhz: 437.500}]
       },
       %{
         name: "RS-44",
         number: 44909,
         modulation: :linear,
-        downlinks: [%{lower_mhz: 435.610, upper_mhz: 435.670}]
+        downlinks: [%{lower_mhz: 435.610, upper_mhz: 435.670}],
+        uplinks: [%{lower_mhz: 145.935, upper_mhz: 145.995}]
       },
       %{
         name: "SO-50",
         number: 27607,
         modulation: :fm,
-        downlinks: [%{lower_mhz: 436.795, upper_mhz: 436.795}]
+        downlinks: [%{lower_mhz: 436.795, upper_mhz: 436.795}],
+        uplinks: [%{lower_mhz: 145.850, upper_mhz: 145.850}]
       },
       %{
         name: "TO-108",
         number: 44881,
         modulation: :linear,
-        downlinks: [%{lower_mhz: 145.915, upper_mhz: 145.935}]
+        downlinks: [%{lower_mhz: 145.915, upper_mhz: 145.935}],
+        uplinks: [%{lower_mhz: 435.270, upper_mhz: 435.290}]
       },
       %{
         name: "XW-2A",
         number: 40903,
         modulation: :linear,
-        downlinks: [%{lower_mhz: 145.665, upper_mhz: 145.685}]
+        downlinks: [%{lower_mhz: 145.665, upper_mhz: 145.685}],
+        uplinks: [%{lower_mhz: 435.030, upper_mhz: 435.050}]
       },
       %{
         name: "XW-2C",
         number: 40906,
         modulation: :linear,
-        downlinks: [%{lower_mhz: 145.795, upper_mhz: 145.815}]
+        downlinks: [%{lower_mhz: 145.795, upper_mhz: 145.815}],
+        uplinks: [%{lower_mhz: 435.150, upper_mhz: 435.170}]
       },
       %{
         name: "EO-88",
         number: 42017,
         modulation: :linear,
-        downlinks: [%{lower_mhz: 145.960, upper_mhz: 145.990}]
+        downlinks: [%{lower_mhz: 145.960, upper_mhz: 145.990}],
+        uplinks: [%{lower_mhz: 435.015, upper_mhz: 435.045}]
       }
     ]
     |> Enum.map(&Map.put_new(&1, :slug, &1.name))
