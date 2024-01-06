c4eda7290ce4aa4bb34cb29317a798388a11cef1
Rockwell Schrock <rockwell@schrock.me>
2024-01-05 21:55:33 -0500

    Add logo

--- lib/hamsat_web/views/page_view.ex
+++ lib/hamsat_web/views/page_view.ex
@@ -4,7 +4,7 @@ defmodule HamsatWeb.PageView do
   def date_header(assigns) do
     ~H"""
     <a href={"##{@date}"} name={@date}>
-      <h2 class="text-h3 md:text-h2 mt-8 text-gray-400 mb-1 border-b"><%= @date %></h2>
+      <h2 class="text-h3 md:text-h2 mt-8 text-gray-400 mb-2 pb-1 border-b"><%= @date %></h2>
     </a>
     """
   end
