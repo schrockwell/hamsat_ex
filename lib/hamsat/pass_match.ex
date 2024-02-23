60f65cb8248881bc53c4fea4b699d55c2272a010
Rockwell Schrock <rockwell@schrock.me>
2024-02-23 18:02:36 -0500

    Some light PassMatch/PassPlot refactoring

--- lib/hamsat/pass_match.ex
+++ lib/hamsat/pass_match.ex
@@ -1,8 +1,29 @@
 defmodule Hamsat.PassMatch do
   defstruct [:sat, :plots, :start_time, :end_time]
 
+  alias Hamsat.Coord
+
   defmodule PassPlot do
-    defstruct [:location, :pass, coords: []]
+    defstruct [:location, :pass, :satrec, coords: []]
+
+    def populate_coords(%__MODULE__{} = plot, opts \\ []) do
+      start_time = opts[:start_time] || Timex.to_datetime(plot.pass.start_time)
+      end_time = opts[:end_time] || Timex.to_datetime(plot.pass.end_time)
+      num_points = opts[:points] || 40
+
+      observer = Coord.to_observer(plot.location)
+      duration = Timex.diff(end_time, start_time, :seconds)
+      step = duration / num_points
+
+      coords =
+        Enum.map(0..num_points, fn i ->
+          time = Timex.shift(start_time, seconds: trunc(i * step))
+          pos = Satellite.Passes.current_position(plot.satrec, observer, Timex.to_erl(time), magnitude?: false)
+          %{az: pos.azimuth_in_degrees, el: pos.elevation_in_degrees}
+        end)
+
+      %{plot | coords: coords} |> IO.inspect()
+    end
   end
 
   alias Hamsat.Coord
@@ -22,14 +43,10 @@ defmodule Hamsat.PassMatch do
     observer = Coord.to_observer(location)
     pass = Satellite.next_pass(satrec, Timex.to_erl(time), observer, magnitude?: false)
 
-    %PassPlot{location: location, pass: pass}
+    %PassPlot{satrec: satrec, location: location, pass: pass}
   end
 
   defp populate_coords(pass_match, opts) do
-    points = opts[:points] || 40
-
-    satrec = Sat.get_satrec(pass_match.sat)
-
     match_start =
       pass_match.plots
       |> Enum.map(& &1.pass.start_time)
@@ -45,23 +62,14 @@ defmodule Hamsat.PassMatch do
     pass_match = %{pass_match | start_time: match_start, end_time: match_end}
 
     duration = Timex.diff(match_end, match_start, :seconds)
-    step = duration / points
 
     if duration <= 0 do
       pass_match
     else
       new_plots =
         for plot <- pass_match.plots do
-          observer = Coord.to_observer(plot.location)
-
-          coords =
-            Enum.map(0..points, fn i ->
-              time = Timex.shift(match_start, seconds: trunc(i * step))
-              pos = Satellite.Passes.current_position(satrec, observer, Timex.to_erl(time), magnitude?: false)
-              %{az: pos.azimuth_in_degrees, el: pos.elevation_in_degrees}
-            end)
-
-          %{plot | coords: coords}
+          populate_opts = Keyword.merge(opts, start_time: match_start, end_time: match_end)
+          PassPlot.populate_coords(plot, populate_opts)
         end
 
       %{pass_match | plots: new_plots}
